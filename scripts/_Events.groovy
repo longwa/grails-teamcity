1f98ebe4b834897370e159e028d9a03b893cdd31
Aaron Long <along@triu.com>
2012-06-30 17:36:08 -0400

    Add test hooks

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,7 +1,75 @@
-import grails.teamcity.ServiceMessage
-
 eventTestSuiteStart = { name ->
-    def message = new ServiceMessage("testSuiteStarted")
-    message.attributes.name = name
-    println message.toString()
+    if( isEnabled() ) {
+        def msg = loadMessageClass("ServiceMessage").newInstance("testSuiteStarted")
+        msg.name = name
+        println msg.toString()
+    }
 }
+
+eventTestSuiteEnd = { name ->
+    if( isEnabled() ) {
+        def msg = loadMessageClass("ServiceMessage").newInstance("testSuiteEnded")
+        msg.name = name
+        println msg.toString()
+    }
+}
+
+// No message in TeamCity for these, just need to get information for use in the other messages
+def currentTestCaseName = ""
+eventTestCaseStart = { testCaseName ->
+    currentTestCaseName = testCaseName
+}
+
+eventTestCaseEnd = { testCaseName, out, err ->
+    currentTestCaseName = ""
+}
+
+eventTestStart = { testName ->
+    if( isEnabled() ) {
+        def msg = loadMessageClass("ServiceMessage").newInstance("testStarted")
+        msg.name = "${currentTestCaseName}.${testName}"
+        println msg.toString()
+    }
+}
+
+eventTestFailure = { testName, failure, isError ->
+    if( isEnabled() ) {
+        def msg = loadMessageClass("ServiceMessage").newInstance("testFailed")
+        msg.name = "${currentTestCaseName}.${testName}"
+        if( failure instanceof Throwable ) {
+            msg.message = failure.message
+            msg.details = failure
+        }
+        else {
+            msg.message = failure
+            msg.details = failure
+        }
+        println msg.toString()
+    }
+}
+
+eventTestEnd = { testName ->
+    if( isEnabled() ) {
+        def msg = loadMessageClass("ServiceMessage").newInstance("testFinished")
+        msg.name = "${currentTestCaseName}.${testName}"
+        println msg.toString()
+    }
+}
+
+// Make sure the classes are loaded and compile if needed
+loadMessageClass = { className ->
+    def doLoad = {-> classLoader.loadClass("grails.teamcity.${className}") }
+    try {
+        doLoad()
+    }
+    catch(ClassNotFoundException ignored) {
+        includeTargets << grailsScript("_GrailsCompile")
+        compile()
+        doLoad()
+    }
+}
+
+isEnabled = {
+    System.getProperty("teamcity.version") != null
+}
+
