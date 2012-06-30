12fa0ab507f554bfecfa6833414c98b70d9a8539
Aaron Long <along@triu.com>
2012-06-30 17:36:30 -0400

    Add property shorthand and escaping strings

--- src/groovy/grails/teamcity/ServiceMessage.groovy
+++ src/groovy/grails/teamcity/ServiceMessage.groovy
@@ -10,6 +10,7 @@ class ServiceMessage {
 
     def attributes = [:] as TreeMap
     def messageName
+    def enabled
 
     ServiceMessage(def name) {
         messageName = name
@@ -17,18 +18,26 @@ class ServiceMessage {
         // Default attributes
         attributes.flowId = "grails"
         attributes.timestamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").format(new Date())
+
+    }
+
+    def propertyMissing(String name, value) {
+        attributes[name] = value
+    }
+
+    def propertyMissing(String name) {
+        attributes[name]
     }
 
     /**
      * Format the message
      */
     String toString() {
-        def attrString = attributes.collect { k, v -> "${k}='${escape(v)}'" }.join(" ")
+        def attrString = attributes.collect { k, v -> "${k}='${escape(v?.toString())}'" }.join(" ")
         "##teamcity[${messageName} ${attrString}]"
     }
 
     private String escape(String str) {
-        // TODO - Need to escape some things with "|"
-        str
+        str.replaceAll("\\|", "||").replaceAll("\n", "|n").replaceAll("'", "|'")
     }
 }
--- test/unit/grails/teamcity/ServiceMessageTests.groovy
+++ test/unit/grails/teamcity/ServiceMessageTests.groovy
@@ -10,5 +10,42 @@ class ServiceMessageTests extends GroovyTestCase {
 
         assert output != null
         assert output == "##teamcity[test flowId='grails' foo='bar' timestamp='now']"
+        println output
     }
+
+    void testEscapeStrings() {
+        def message = new ServiceMessage("test")
+        message.attributes.timestamp = 'now'
+        message.attributes.multi = 'Line1\nLine2'
+        message.attributes.apost = "It's hot"
+        message.attributes.bar   = "Assert |"
+
+        def output = message.toString()
+
+        assert output != null
+        assert output == "##teamcity[test apost='It|'s hot' bar='Assert ||' flowId='grails' multi='Line1|nLine2' timestamp='now']"
+        println output
+    }
+
+    void testPropertyAccess() {
+        def message = new ServiceMessage("test")
+        message.timestamp = 'now'
+        message.foo = 'bar'
+
+        def output = message.toString()
+
+        assert output != null
+        assert output == "##teamcity[test flowId='grails' foo='bar' timestamp='now']"
+        println output
+    }
+
+    /*
+    void testFailure() {
+        throw new Exception("Failed")
+    }
+
+    void testAssertionFailure() {
+        assert 1 == 2
+    }
+    */
 }
