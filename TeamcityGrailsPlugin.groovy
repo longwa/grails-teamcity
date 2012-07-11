84931b7e30ad5b091bb351c0d5694483839fa27d
Aaron Long <along@triu.com>
2012-07-11 10:36:41 -0400

    Update plugin information

--- TeamcityGrailsPlugin.groovy
+++ TeamcityGrailsPlugin.groovy
@@ -1,18 +1,18 @@
 class TeamcityGrailsPlugin {
-    def version = "1.0.2"
+    def version = "1.0.3"
     def grailsVersion = "2.0 > *"
     def dependsOn = [:]
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
 
-    def title = "Teamcity Integration" // Headline display name of the plugin
+    def title = "TeamCity Integration"
     def author = "Aaron Long"
     def authorEmail = "longwa@gmail.com"
     def description = 'Provides build status and test run integration with TeamCity'
 
-    def documentation = "http://grails.org/plugin/teamcity"
+    def documentation = "https://github.com/longwa/grails-teamcity"
     def license = "APACHE"
-    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-    def scm = [ url: "fixme" ]
+    def issueManagement = [ url: "https://github.com/longwa/grails-teamcity/issues" ]
+    def scm = [ url: "https://github.com/longwa/grails-teamcity" ]
 }
